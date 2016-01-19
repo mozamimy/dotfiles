729cd2206a9e2ce720469bd6dba7888b7dfe77ef
Moza USANE <yuma-asada@cookpad.com>
2016-01-19 10:41:02 +0900

    Use binding ip for boot.sh

--- sh/boot.sh
+++ sh/boot.sh
@@ -1,5 +1,5 @@
 if [ $1 = "cookpad" ]; then
   USE_SSO_SERVER=1 bundle exec foreman start -m "memcached=1,ttserver=1,redis=1,rails=0,rails_sso=1" &
-  bundle exec rails s
+  bundle exec rails s -b 0.0.0.0
   return
 fi
